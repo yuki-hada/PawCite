7eeb4366a4ff9bf2f9c451d700dbbf6e0eb1d258
strelka <yuki-hada@g.ecc.u-tokyo.ac.jp>
2025-07-26 01:54:57 +0900

    🚀First commit

---
+++ pawcite.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "yuki"
+description   = "A new awesome nimble package"
+license       = "GPL-3.0-or-later"
+srcDir        = "src"
+bin           = @["pawcite"]
+
+
+# Dependencies
+
+requires "nim >= 2.2.4"
---
+++ src/pawcite.nim
@@ -0,0 +1,36 @@
+import httpclient, json, strformat, strutils, parseopt
+
+proc fetchJson*(url: string): JsonNode =
+    let client = newHttpClient()
+    let resp = client.get(url)
+    if resp.code != Http200:
+        raise newException(ValueError, &"HTTP error: {resp.status}")
+    return parseJson(resp.body)
+
+proc format_crossref_citation*(doi: string): string =
+    let data    = fetchJson("https://api.crossref.org/works/" & doi)
+    let authors = data["message"]["author"]
+    var firstAuthor: JsonNode
+
+    for author in authors:
+        if author["sequence"].getStr == "first":
+            firstAuthor = author
+            break
+
+    let etal = if authors.len > 1: " et al." else: ""
+    let yearStr = $data["message"]["published-online"]["date-parts"][0][0].getInt
+    result = firstAuthor["family"].getStr &
+                " " & toUpperAscii(firstAuthor["given"].getStr[0]) & "." &
+                etal & ", " &
+                data["message"]["short-container-title"][0].getStr & ". (" &
+                yearStr & ")"
+
+when isMainModule:
+    var p = initOptParser()
+    while true:
+        p.next()
+        case p.kind
+        of cmdEnd: break
+        of cmdArgument:
+            echo format_crossref_citation(p.key)
+        else: discard
